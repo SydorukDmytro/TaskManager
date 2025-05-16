bcdbb2837085b9cbe8e386a36ba2d041f3dec14c
Hosakoo <hosakoo2020@gmail.com>
2025-05-16 03:32:57 +0300

    Full Name is used in navbar

---
+++ ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+
+namespace TaskManager.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserNameViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return View<string>("Default", null);
+
+            var user = await _userManager.GetUserAsync(UserClaimsPrincipal);
+            var fullName = user?.FullName ?? user?.Email;
+
+            return View("Default", fullName);
+        }
+    }
+}
